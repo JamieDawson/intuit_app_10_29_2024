a4e8fe84a1856921e15e07ec318067d6ac0c6a0c
Jamie Dawson <jamierobertdawson@gmail.com>
2024-10-29 08:04:10 -0700

    starting project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import InvoiceWrapper from "./Components/InvoiceWrapper/InvoiceWrapper.component";
+
+function App() {
+  return (
+    <div className="App">
+      <div>Intuit app</div>
+    </div>
+  );
+}
+
+export default App;
